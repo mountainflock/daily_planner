9219bdaccedeba7421d516da60061d5f00459d78
Marina <reddymikks@gmail.noreply.github.com.>
2025-01-30 22:06:52 +0300

    Add morning tasks array to the board

---
+++ src/components/morningBoard/morningBoard.jsx
@@ -0,0 +1,32 @@
+import "./morningBoard.css";
+
+const CardList = () => {
+  const morningTodos = [
+    "Пробуждение",
+    "Вода",
+    "6 минут",
+    "Молитва",
+    "Фото",
+    "Постель",
+    "Переодеваюсь",
+    "Умываюсь",
+    "Зарядка",
+    "Завтрак",
+    "Душ",
+    "Ревитоника",
+    "Отдых, медитация",
+    "Планер",
+  ];
+
+  return (
+    <div className="morningCards">
+      {morningTodos.map((todo, i) => (
+        <div className="morningTodo" key={morningTodos[i]}>
+          {todo}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
