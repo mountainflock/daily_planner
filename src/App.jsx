7c42d2a34fef834a9a2fdc942de20bb0d0df3444
Marina <reddymikks@gmail.noreply.github.com.>
2025-02-01 18:47:59 +0300

    Replace divs with dayly tasks with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Board from "./components/board/board";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <Board />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
   );
 }
 
 export default App;
+{
+  /* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div> */
+}
